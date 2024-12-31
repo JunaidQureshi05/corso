f7c648140368d2cb2517bd133329c1bdf3a402f2
Junaid Qureshi <Junaid.562000@gmail.com>
2024-12-31 14:15:35 +0530

    done util details 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // Add custom JavaScript here
+
+function addEventListeners() {
+  let nav = document.querySelector("nav");
+  if (window.innerWidth <= 500) {
+    nav.classList.add("bg-dark");
+  }
+
+  window.addEventListener("scroll", () => {
+    console.log(window.scrollY > 50);
+
+    if (window.scrollY > 50) {
+      nav.classList.add("bg-dark");
+    } else {
+      nav.classList.remove("bg-dark");
+    }
+  });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth <= 500) {
+      nav.classList.add("bg-dark");
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", addEventListeners);
